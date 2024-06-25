0fa32b399a2252e0bff39d1b28e6371c24d4c3a7
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-06-25 15:48:04 +0200

    rename random_fun to random_val and changes according to the change request

--- src/statement_generator.cpp
+++ src/statement_generator.cpp
@@ -521,7 +521,7 @@ unique_ptr<TableRef> StatementGenerator::GenerateTableFunctionRef() {
 	auto random_val = RandomValue(original_val);
 	auto table_function_ref = &generator_context->table_functions[random_val];
 	while (table_function_ref->get().type == CatalogType::TABLE_MACRO_ENTRY) {
-		random_val +=1 % generator_context->table_functions.size();
+		random_val += 1 % original_val;
 		if (random_val == original_val) {
 			throw InternalException("No table_functions to test.");
 		}
