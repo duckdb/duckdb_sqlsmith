73b1cf10c5947f63dcd1b87330b468177a3984fc
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-09-26 15:38:51 +0200

    parseJson recursively

--- src/random_nums_config.cpp
+++ src/random_nums_config.cpp
@@ -54,71 +54,43 @@ enum Statements {
 
 };
 
+void ParseJsonObj(yyjson_val *obj, unordered_map<RandomPercentagesEnum, idx_t> &config_from_file) {
+    yyjson_obj_iter iter;
+    yyjson_obj_iter_init(obj, &iter);
+    size_t idx, max;
+    yyjson_val *key, *val;
+    yyjson_obj_foreach(obj, idx, max, key, val) {
+        const char* root_key = yyjson_get_str(key);
+        auto it = StringToRandomPercentagesEnum.find(root_key);
+        if (it != StringToRandomPercentagesEnum.end()) {
+            RandomPercentagesEnum perc_type = it->second;
+            auto perc_value = yyjson_get_str(val);
+            if (perc_value) {
+                config_from_file[perc_type] = std::stoi(perc_value);
+            }
+        }
+        if (yyjson_is_obj(val)) {
+            ParseJsonObj(val, config_from_file);
+        }
+    }
+}
 
 unordered_map<RandomPercentagesEnum, idx_t> GetConfigFromFile(const char *json_string) {
+    
     unordered_map<RandomPercentagesEnum, idx_t> config_from_file;
     auto doc = yyjson_read_file(json_string, YYJSON_READ_NOFLAG, NULL, NULL);
     if (doc) {
-        yyjson_val *obj = yyjson_doc_get_root(doc);
-        yyjson_obj_iter iter;
-        yyjson_obj_iter_init(obj, &iter);
-        size_t idx, max;
-        yyjson_val *key, *val;
-        yyjson_obj_foreach(obj, idx, max, key, val) {
-            const char* root_key = yyjson_get_str(key);
-            auto it = StringToRandomPercentagesEnum.find(root_key); // "select" or "attach"
-            if (it != StringToRandomPercentagesEnum.end()) {
-                RandomPercentagesEnum perc_type = it->second; // SELECT
-                auto perc_value = yyjson_get_str(val);
-                config_from_file[perc_type] = std::stoi(perc_value); // { SELECT: 90 }
-            } 
-            if (yyjson_is_obj(val)) {
-                size_t node_idx, node_max;
-                yyjson_val *node_key, *node_val;
-                yyjson_obj_foreach(val, node_idx, node_max, node_key, node_val) {
-                    const char* node_root_key = yyjson_get_str(node_key); // sub roots are "select_node" and "select_node_sample"
-                    auto node_it = StringToRandomPercentagesEnum.find(node_root_key);
-                    if (node_it != StringToRandomPercentagesEnum.end()) {
-                        RandomPercentagesEnum node_perc_type = node_it->second;
-                        auto node_perc_value = yyjson_get_str(node_val);
-                        config_from_file[node_perc_type] = std::stoi(node_perc_value);
-                    }
-                    if (yyjson_is_obj(node_val)) {
-                        size_t sub_idx, sub_max;
-                        yyjson_val *sub_key, *sub_val;
-                        yyjson_obj_foreach(node_val, sub_idx, sub_max, sub_key, sub_val) {
-                            const char* sub_root_key = yyjson_get_str(sub_key);
-                            auto sub_it = StringToRandomPercentagesEnum.find(sub_root_key);
-                            if (sub_it != StringToRandomPercentagesEnum.end()) {
-                                RandomPercentagesEnum sub_perc_type = sub_it->second;
-                                auto sub_perc_value = yyjson_get_str(sub_val);
-                                config_from_file[sub_perc_type] = std::stoi(sub_perc_value);
-                            }
-                            if (yyjson_is_obj(sub_val)) {
-                                size_t subnode_idx, subnode_max;
-                                yyjson_val *subnode_key, *subnode_val;
-                                yyjson_obj_foreach(sub_val, subnode_idx, subnode_max, subnode_key, subnode_val) {
-                                    const char* subnode_root_key = yyjson_get_str(subnode_key);
-                                    auto subnode_it = StringToRandomPercentagesEnum.find(subnode_root_key);
-                                    if (subnode_it != StringToRandomPercentagesEnum.end()) {
-                                        RandomPercentagesEnum subnode_perc_type = subnode_it->second;
-                                        auto subnode_perc_value = yyjson_get_str(subnode_val);
-                                        config_from_file[subnode_perc_type] = std::stoi(subnode_perc_value);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        yyjson_val *root = yyjson_doc_get_root(doc);
+        if (yyjson_is_obj(root)) {
+            ParseJsonObj(root, config_from_file);
         }
+        // Free the doc
+        yyjson_doc_free(doc);
     } else {
         // Couldn't read JSON with percentages config
         yyjson_doc_free(doc);
         return GetDefaultConfig();
     }
-        // Free the doc
-    yyjson_doc_free(doc);
     return config_from_file;
 }
 } // namespace duckdb
